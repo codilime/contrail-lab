beff2fd3b44cfcf466d9541e047d30ce8fec966c
Przemysław Marcinkiewicz <przemyslaw.marcinkiewicz@codilime.com>
2018-08-10 16:20:05 +0200

    New version with choose orchestrator option.

---
+++ provision/openstack/openstack.tf
@@ -0,0 +1,289 @@
+provider "openstack" {
+  user_name           = "${var.user_name}"
+  project_domain_name = "${var.domain_name}"
+  password            = "${var.password}"
+  auth_url            = "https://stack.intra.codilime.com:5000/v3"
+  user_domain_name    = "${var.domain_name}"
+  tenant_id           = "${var.project_id}"
+}
+
+resource "openstack_compute_keypair_v2" "KeyPair" {
+  name       = "${replace(var.user_name,".","-")}-KeyPair"
+  public_key = "${file("${var.ssh_key_file}")}"
+
+}
+
+resource "openstack_compute_secgroup_v2" "contrail_security_group" {
+  name        = "${var.user_name}-contrail_security_group"
+  description = "Security Group for contrail_net"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8143
+    to_port     = 8143
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 3306
+    to_port     = 3306
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 5673
+    to_port     = 5673
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 6379
+    to_port     = 6379
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 35357
+    to_port     = 35357
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 11211
+    to_port     = 11211
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 4369
+    to_port     = 4369
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8083
+    to_port     = 8083
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8774
+    to_port     = 8774
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8780
+    to_port     = 8780
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 5050
+    to_port     = 5050
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 6385
+    to_port     = 6385
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 9292
+    to_port     = 9292
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8080
+    to_port     = 8080
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8086
+    to_port     = 8086
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 2181
+    to_port     = 2181
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 6080
+    to_port     = 6080
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_instance_v2" "basic" {
+  name            = "${var.user_name}"
+  image_id        = "d1ccf955-b11a-4a68-b578-8255367f7f9b"
+  flavor_name     = "m2.huge"
+  key_pair        = "${openstack_compute_keypair_v2.KeyPair.id}"
+  security_groups = ["${openstack_compute_secgroup_v2.contrail_security_group.id}"]
+
+  network {
+    name = "${var.network_name}"
+  }
+}
+
+resource "openstack_networking_floatingip_v2" "floatip_1" {
+  pool = "public"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "floatip_1" {
+  floating_ip = "${openstack_networking_floatingip_v2.floatip_1.address}"
+  instance_id = "${openstack_compute_instance_v2.basic.id}"
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "centos"
+      password    = ""
+      agent       = "false"
+      host        = "${openstack_networking_floatingip_v2.floatip_1.address}"
+      private_key = "${file(var.ssh_private_key)}"
+      timeout     = "5m"
+    }
+
+    inline = [
+      "sudo yum -y install kernel-devel kernel-headers ansible git",
+      "sudo git clone http://github.com/Juniper/contrail-ansible-deployer -b ${var.branch}",
+      "sudo mkdir /etc/docker",
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "./prepare_template ${openstack_compute_instance_v2.basic.network.0.fixed_ip_v4} ${var.routerip}"
+  }
+
+  provisioner "file" {
+    source      = "daemon.json"
+    destination = "/tmp/daemon.json"
+
+    connection {
+      type        = "ssh"
+      agent       = "false"
+      user        = "centos"
+      host        = "${openstack_networking_floatingip_v2.floatip_1.address}"
+      private_key = "${file(var.ssh_private_key)}"
+    }
+  }
+
+  provisioner "file" {
+    source      = "${var.ssh_private_key}"
+    destination = "/tmp/id_rsa"
+
+    connection {
+      type        = "ssh"
+      agent       = "false"
+      user        = "centos"
+      host        = "${openstack_networking_floatingip_v2.floatip_1.address}"
+      private_key = "${file(var.ssh_private_key)}"
+    }
+  }
+
+  provisioner "file" {
+    source      = "instances.yaml"
+    destination = "/tmp/instances.yaml"
+
+    connection {
+      type        = "ssh"
+      agent       = "false"
+      user        = "centos"
+      host        = "${openstack_networking_floatingip_v2.floatip_1.address}"
+      private_key = "${file(var.ssh_private_key)}"
+    }
+  }
+
+ 
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "centos"
+      password    = ""
+      agent       = "false"
+      host        = "${openstack_networking_floatingip_v2.floatip_1.address}"
+      private_key = "${file(var.ssh_private_key)}"
+      timeout     = "3m"
+    }
+
+    inline = [
+      "sudo cp /tmp/daemon.json /etc/docker/",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "centos"
+      password    = ""
+      agent       = "false"
+      host        = "${openstack_networking_floatingip_v2.floatip_1.address}"
+      private_key = "${file(var.ssh_private_key)}"
+      timeout     = "5m"
+    }
+
+    inline = [
+      "cd /home/centos",
+      "sudo cp /tmp/instances.yaml /home/centos/contrail-ansible-deployer/config/",
+      "sudo cp /tmp/id_rsa /home/centos/",
+      "sudo chmod +x /usr/local/bin/vrouter.sh",
+      "sudo chmod 600 /home/centos/id_rsa",
+      "cd contrail-ansible-deployer",
+      "sudo ansible-playbook -e orchestrator=openstack -i inventory/ playbooks/configure_instances.yml",
+      "sudo ansible-playbook -i inventory playbooks/install_openstack.yml -v",
+      "sudo ansible-playbook -i inventory/ -e orchestrator=openstack playbooks/install_contrail.yml",
+      "echo ${openstack_compute_instance_v2.basic.network.0.fixed_ip_v4} $HOSTNAME | sudo tee --append /etc/hosts",
+      "sudo reboot",
+    ]
+  }
+}
+
+output "ip" {
+  value = "${openstack_networking_floatingip_v2.floatip_1.address}"
+}
+
+output "localip" {
+  value = "${openstack_compute_instance_v2.basic.network.0.fixed_ip_v4}"
+}
+
+output "instance ID" {
+  value = "${openstack_compute_instance_v2.basic.id}"
+}
--- provision/vrouter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker restart vrouter_vrouter-kernel-init_1
-sleep 40
-docker restart vrouter_vrouter-agent_1
